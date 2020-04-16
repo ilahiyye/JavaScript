1a04b207efe6e42737ae4a649561ac02a829db23
ilahiyye <ilahiyye@gmail.com>
2020-04-16 10:09:54 +0400

    secont commit

---
+++ js/new.js
@@ -0,0 +1,18 @@
+var q = confirm("Do you eat steak?")
+if(q){
+    
+    age = prompt("What's your age?");
+    if(age<21){
+        alert ("You can't buy steak!");
+    }else{
+        alert("Here's your steak");
+    }
+
+} else {
+    alert("Here's fry and ketchup!");
+    
+}
+ 
+
+
+
